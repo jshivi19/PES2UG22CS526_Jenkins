1315a7f822bc5e499ad3c38b57e2c1dc1e2f03ad
jshivi19 <jshivi575@gmail.com>
2025-03-11 16:10:43 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello from , Shivi_PES2UG22CS526\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
